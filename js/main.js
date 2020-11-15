1e517caa83e71bf161817f6a0229c5ff172dda43
Jcdiem <jcdiem@protonmail.com>
2020-11-14 22:52:58 -0600

    Fancy stuff

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(
+    function () {
+        // event handlers
+        $("#tabs a").click(showTab);
+
+
+        // other functions
+        function showTab(event) {
+            event.preventDefault();
+            $(this).tab("show");
+        }
+    });
